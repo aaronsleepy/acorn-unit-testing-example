37ba37fd16db2833fef90bd4ed9b5b0c501add48
aaronsleepy <aaron@kmong.com>
2022-08-15 21:54:57 +0900

    test: Add mockk dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.3.0")
-
+    testImplementation("io.mockk:mockk:1.12.4")
 }
 
 tasks.test {
@@ -22,5 +22,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
 }
