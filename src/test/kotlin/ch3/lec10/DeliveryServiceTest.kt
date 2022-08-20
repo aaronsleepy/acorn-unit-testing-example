90733d9c325a5cd5c00b711534aae96ea38a0ab8
aaronsleepy <aaron@kmong.com>
2022-08-20 18:13:12 +0900

    refactoring: 기존 테스트 유지

--- src/test/kotlin/ch3/lec10/DeliveryServiceTest.kt
+++ src/test/kotlin/ch3/lec10/DeliveryServiceTest.kt
@@ -5,6 +5,26 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
 class DeliveryServiceTest {
+    /**
+     * 모호한 테스트 이름
+     */
+    @Test
+    fun isDeliveryValid_invalidDate_returnsFalse() {
+        // given-Arrange
+        val sut = DeliveryService()
+        val pastDate = LocalDateTime.now().minusDays(1)
+        val delivery = Delivery(pastDate)
+
+        // when-Act
+        val isValid = sut.isDeliveryValid(delivery)
+
+        // then-Assert
+        assertThat(isValid).isFalse
+    }
+
+    /**
+     * 더 나은 테스트 이름
+     */
     @Test
     fun delivery_with_a_past_date_is_invalid() {
         // given-Arrange
