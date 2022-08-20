fcb2f0c4d661b75945861c822f1135ef1c5a76a0
aaronsleepy <aaron@kmong.com>
2022-08-20 18:15:01 +0900

    refactoring: 더 나은 한글 테스트 명명법 with @DisplayName

--- src/test/kotlin/ch3/lec10/DeliveryServiceTest.kt
+++ src/test/kotlin/ch3/lec10/DeliveryServiceTest.kt
@@ -1,6 +1,7 @@
 package ch3.lec10
 
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 
@@ -27,6 +28,24 @@ class DeliveryServiceTest {
      */
     @Test
     fun delivery_with_a_past_date_is_invalid() {
+        // given-Arrange
+        val sut = DeliveryService()
+        val pastDate = LocalDateTime.now().minusDays(1)
+        val delivery = Delivery(pastDate)
+
+        // when-Act
+        val isValid = sut.isDeliveryValid(delivery)
+
+        // then-Assert
+        assertThat(isValid).isFalse
+    }
+
+    /**
+     * 더 나은 한글 테스트 명명법 with @DisplayName
+     */
+    @Test
+    @DisplayName("과거 날짜의 Delivery는 유효하지 않다")
+    fun any_meaningful_test_name() {
         // given-Arrange
         val sut = DeliveryService()
         val pastDate = LocalDateTime.now().minusDays(1)
