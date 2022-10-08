767c42af486ac7f4b2bf3d8f50ab43790d87be57
aaronsleepy <aaron@kmong.com>
2022-10-08 21:47:26 +0900

    refactoring: 기본 생성자 추가

--- src/main/kotlin/ch7/lec02/refactor02/Company.kt
+++ src/main/kotlin/ch7/lec02/refactor02/Company.kt
@@ -4,16 +4,18 @@ package ch7.lec02.refactor02
  * 예제 7.4 도메인 계층의 새로운 클래스
  */
 class Company(
+    var _domainName: String = "",
+    var _numberOfEmployees: Int = 0,
 ) {
-    var domainName: String = ""
-        private set
+    val domainName
+        get() = this._domainName
 
-    var numberOfEmployees: Int = 0
-        private set
+    val numberOfEmployees
+        get() = this._numberOfEmployees
 
     fun changeNumberOfEmployees(delta: Int) {
         assert(numberOfEmployees + delta >= 0)
-        numberOfEmployees += delta
+        _numberOfEmployees += delta
     }
 
     fun isEmailCorporate(email: String): Boolean {
